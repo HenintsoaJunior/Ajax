b90ee88bb9423d95c5577f2d4a42994dd2bdb886
Henintsoa Rakotoarimanana <143500070+HenintsoaJunior@users.noreply.github.com>
2023-11-17 15:29:10 +0300

    Update json.php

--- exo1/json.php
+++ exo1/json.php
@@ -13,3 +13,31 @@
   
 
 ?>
+
+
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'login.php', true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        if (response.success) {
+          // Rediriger ou afficher un message de succès
+        } else {
+          document.getElementById('error').innerText = 'Login incorrect';
+        }
+      } else {
+        document.getElementById('error').innerText = 'Erreur de connexion';
+      }
+    }
+  };
+  const data = `email=${email}&password=${password}`;
+  xhr.send(data);
+});
